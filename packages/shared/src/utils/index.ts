37bbc46c286ddda0020835f35525b587c8e87ab1
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-05-11 21:37:16 +0530

    refactor(utils): comment out unused utility exports

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,2 +1,4 @@
-export * from "./zod-validation.js";
-export * from "./logger.js";
+// export * from "./zod-validation.js";
+// export * from "./logger.js";
+// export * from "./password.js";
+// export * from "./getUniqueUsername.js";
