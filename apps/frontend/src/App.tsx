3eba87dd9e6e875524028df9291c60d27c8bc8c1
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-05-19 20:08:30 +0530

    feat(client): init shadcn ui

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from "./components/ui/8bit/button";
+
 function App() {
 	return (
-		<>
+		<div className="">
 			<h1>Welcome to Khel Mitra</h1>
-		</>
+
+			<Button variant="default">Button</Button>
+		</div>
 	);
 }
 
