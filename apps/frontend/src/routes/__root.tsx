e38e393d20fc3da258c5cb2f4571e876bfc4445d
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-05-23 07:52:47 +0530

    feat(toast): add custom toast component and integrate sonner for notifications

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,22 +1,24 @@
+import { Toaster } from "@/components/ui/sonner";
 import type { IAuthContext } from "@/types/auth.context.type";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 interface RouterContext {
-	auth: IAuthContext;
+    auth: IAuthContext;
 }
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-	component: RootComponent,
+    component: RootComponent,
 });
 
 function RootComponent() {
-	return (
-		<>
-			<div className="w-full min-h-screen mx-auto">
-				<Outlet />
-			</div>
-			<TanStackRouterDevtools />
-		</>
-	);
+    return (
+        <>
+            <div className="w-full min-h-screen mx-auto">
+                <Outlet />
+                <Toaster />
+            </div>
+            <TanStackRouterDevtools />
+        </>
+    );
 }
