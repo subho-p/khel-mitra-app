e09f24efa691555e6d2f04a57a4e827f4446a467
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-05-25 11:19:48 +0530

    fix(drizzle.config): conditionally load dotenv configuration based on NODE_ENV

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-dotenv.config({ path: "./../../.env.local" });
+if (process.env.NODE_ENV !== "production") {
+	dotenv.config({ path: "./../../.env.local" });
+}
 
 if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not defined");
